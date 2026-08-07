333da111857e10e502ffb71300e027361ae8cd3d
leeyanleryan <leeyanleryan.21@gmail.com>
2026-08-07 17:19:26 +0800

    Refactor all css files into globals css file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,5 @@
 import type { AppProps } from "next/app";
-import "../css/default.css";
-import "../css/variables.css";
-import "../css/dark-mode.css";
-import "../css/topbar.css";
-import "../css/sidebar.css";
-import "../css/left-sidebar.css";
-import "../css/right-sidebar.css";
-import "../css/main-body.css";
-import "../css/nav-arrow.css";
-import "../css/clamp-content.css";
-import "../css/media-list.css";
+import "../css/globals.css";
 import Topbar from "../tsx/Topbar";
 import LeftSidebar from "../tsx/LeftSidebar";
 import RightSidebar from "../tsx/RightSidebar";
