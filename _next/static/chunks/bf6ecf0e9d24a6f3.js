eda52b2f59fb4ae4be4184be2c13c48ae7c0d619
leeyanleryan <leeyanleryan@users.noreply.github.com>
2026-08-07 14:07:18 +0000

    deploy: 9f2ad0610b025716b9e13dfecd826972909c082f

--- _next/static/chunks/bf6ecf0e9d24a6f3.js
+++ _next/static/chunks/bf6ecf0e9d24a6f3.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/cd76eb9a1ef91245.js",
   "static/chunks/664b2b7d6bd3fcf5.js",
   "static/chunks/a97974f919caa1a5.js",
-  "static/chunks/4a2a2ce4ad8c0be8.css",
-  "static/chunks/turbopack-88de441c17e24d22.js"
+  "static/chunks/8bb308104cfb67eb.css",
+  "static/chunks/turbopack-1957b5accf247d20.js"
 ])
