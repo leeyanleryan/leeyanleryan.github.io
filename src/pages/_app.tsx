b8abd220f1072ffca4b2ec9104ba0e9e0bff852f
Ryan Lee <leeyanleryan.21@gmail.com>
2026-08-07 20:32:45 +0800

    Switch to Raleway from next/font/google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,26 @@
 import type { AppProps } from "next/app";
+import { Raleway } from "next/font/google";
 import "../css/globals.css";
 import Topbar from "../tsx/Topbar";
 import LeftSidebar from "../tsx/LeftSidebar";
 import RightSidebar from "../tsx/RightSidebar";
 import NavArrow from "../tsx/NavArrow";
 
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={raleway.className}>
       <Topbar />
       <LeftSidebar />
       <RightSidebar />
       <Component {...pageProps} />
       <NavArrow />
       <hr className="hr-hide-1180" />
-    </>
+    </div>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,8 @@ export default function Document() {
         <link rel="icon" href="/favicon/favicon.ico" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
         <script dangerouslySetInnerHTML={{ __html: `(${initUIScript.toString()})()` }} />
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway&display=swap" />
+        {/* <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css" /> */}
+        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway&display=swap" /> */}
         {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" /> */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <meta name="theme-color" content="#252525" />
